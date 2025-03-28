31e64ed1be539c62cd01539c861120d1d71a56da
Thomas Chao <Tchao@lcs.on.ca>
2025-03-28 11:40:01 -0400

    Initial Commit

---
+++ Joke/JokeApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokeApp.swift
+//  Joke
+//
+//  Created by Yuk Yeung Chao on 2025-03-28.
+//
+
+import SwiftUI
+
+@main
+struct JokeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
