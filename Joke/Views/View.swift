093da7416ea57d35b8f1528590f903d485833433
Thomas Chao <Tchao@lcs.on.ca>
2025-03-28 12:08:42 -0400

    Added a function to the view model to retrieve a joke from the view model.

---
+++ Joke/Views/View.swift
@@ -0,0 +1,41 @@
+//
+//  View.swift
+//  Joke
+//
+//  Created by Yuk Yeung Chao on 2025-03-28.
+//
+
+import SwiftUI
+struct JokeView: View {
+    
+    // MARK: Stored properties
+    
+    // Create the view model (temporarily show the default joke)
+    @State var viewModel = JokeViewModel()
+    
+    // MARK: Computed properties
+    var body: some View {
+        VStack {
+            
+            // Show a joke if one exists
+            if let currentJoke = viewModel.currentJoke {
+                
+                Group {
+                    Text(currentJoke.setup ?? "")
+                        .padding(.bottom, 100)
+                    
+                    Text(currentJoke.punchline ?? "")
+ 
+                }
+                .font(.title)
+                .multilineTextAlignment(.center)
+                
+            }
+            
+        }
+    }
+}
+ 
+#Preview {
+    JokeView()
+}
